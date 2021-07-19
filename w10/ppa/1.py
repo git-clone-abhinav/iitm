11a96516c38a4158b6ae48d50c8f3b402de90b61
RaspberryPi <abhi.anu003@gmail.com>
2021-07-20 01:01:15 +0530

    w10ppa1

---
+++ w10/ppa/1.py
@@ -0,0 +1,17 @@
+class Calculator:
+    def Sum(a,b):
+        return a+b
+    def Multiply(a,b):
+        return a*b
+    def Subtraction(a,b):
+        return a-b
+    def Division(a,b):
+        return a/b
+    def Remainder(a,b):
+        return a%b
+    def Power(a,b):
+        return a**b
+    def Quotent(a,b):
+        return a//b
+
+         
